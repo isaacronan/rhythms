ee07fbca1db6ce4997a4ab0d3ad4202f00bf9c6b
Isaac Ronan <isaacronan@users.noreply.github.com>
2023-05-09 10:15:38 -0400

    switch to web audio api

--- src/components/sample-service.tsx
+++ src/components/sample-service.tsx
@@ -1,45 +1,25 @@
-import { PropsWithChildren, createContext, useCallback, useContext, useMemo, useRef, useState } from "react";
+import { PropsWithChildren, createContext, useContext, useEffect, useMemo, useRef } from "react";
 import { ISample, ISampleControls } from "../types";
 
 const SampleContext = createContext<ISampleControls | null>(null);
 
-const BUFFER = 1;
-
-const createBufferedSamples = (samples: ISample[]) => samples.map(sample => new Array(BUFFER).fill(null).map(() => sample)).reduce((acc, cur) => ([...acc, ...cur]));
-
 export const SampleService = (props: PropsWithChildren<{ samples: ISample[] }>) => {
-    const [audioElementSamples, setAudioElementSamples] = useState<ISample[]>(createBufferedSamples(props.samples));
-    const audioElementRefs = useRef<HTMLMediaElement[]>([]);
-    const audioElementSampleRefs = useRef<ISample[]>(createBufferedSamples(props.samples));
+    const sampleBuffers = useRef<AudioBuffer[]>([]);
+    const audioContext = useRef<AudioContext>(new AudioContext());
 
-    const playIndex = (index: number) => {
-        setTimeout(() => audioElementRefs.current[index].play());
-    };
+    useEffect(() => {
+        Promise.all(
+            props.samples.map(sample => fetch(sample.assetPath).then(r => r.arrayBuffer()).then(r => audioContext.current.decodeAudioData(r)))
+        ).then(audioBuffers => sampleBuffers.current = audioBuffers);
+    }, [props.samples]);
 
     const play = (sampleNames: string[]) => {
         sampleNames.forEach(sampleName => {
-            const idleAudioElementIndices = audioElementSampleRefs.current.map((sample, index) => {
-                if (sample.sampleName !== sampleName) return null;
-                const element = audioElementRefs.current[index];
-                if (element === undefined) return null;
-                if (!element.paused && element.duration > 0) return null;
-                return index;
-            }).filter(index => index !== null);
-
-            if (idleAudioElementIndices.length <= BUFFER) {
-                const sample = props.samples.find(sample => sample.sampleName === sampleName);
-                for (let i = 0; i <= BUFFER - idleAudioElementIndices.length; i++) {
-                    setAudioElementSamples(prev => ([...prev, sample!]));
-                    audioElementSampleRefs.current.push(sample!);
-                }
-            }
-            
-            if (idleAudioElementIndices.length > 0) {
-                audioElementRefs.current[idleAudioElementIndices[0]!].play()
-            } else {
-                playIndex(audioElementSampleRefs.current.length - 1);
-            }
-            
+            const index = props.samples.findIndex(sample => sample.sampleName === sampleName);
+            const source = audioContext.current.createBufferSource();
+            source.start(0);
+            source.buffer = sampleBuffers.current[index];
+            source.connect(audioContext.current.destination);
         });
     };
 
@@ -48,17 +28,6 @@ export const SampleService = (props: PropsWithChildren<{ samples: ISample[] }>) 
     return (
         <SampleContext.Provider value={{ play, samples }}>
             {props.children}
-            <div style={{ display: 'none' }}>
-                {audioElementSamples.map((sample, index) => (
-                    <audio key={index} ref={node => {
-                        if (node) {
-                            audioElementRefs.current[index] = node;
-                        } else {
-                            delete audioElementRefs.current[index];
-                        }
-                    }} src={sample.assetPath}></audio>
-                ))}
-            </div>
         </SampleContext.Provider>
     );
 };
