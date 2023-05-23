db20b4da6309edea80998a8889f7643a7927f780
Isaac Ronan <isaacronan@users.noreply.github.com>
2023-05-23 12:12:14 -0400

    wrong public path

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const prodPublicPath = '/rhythms';
+const prodPublicPath = '/rhythms/';
 
 module.exports = (env, argv) => {
     return {
